3265d55e3a3ec63e26de922c90208a54dd2361ca
Hamed Manrique <hmanriquer@sapv.com.mx>
2024-09-05 16:52:48 -0600

    rolledback react query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+'use client'
 import UploadInventory from '@/components/upload-inventory'
 import DataTable from '@/components/data-table'
 import Header from '@/components/header'
-import { get } from '@/actions/items.actions'
+import { useItems } from '@/hooks/items'
 import { Spinner } from '@nextui-org/react'
 import Navigation from '@/components/navigation'
 
-export default async function Page() {
-  const data = await get()
+export default function Page() {
+  const { data, isLoading } = useItems()
+  console.log(data)
 
   return (
     <main className="min-h-screen">
@@ -17,7 +19,13 @@ export default async function Page() {
           description="Revisa o agrega los productos del inventario"
           button={<UploadInventory />}
         />
-        <DataTable data={data as any} />
+        {isLoading || !data ? (
+          <div className="mx-auto flex items-center justify-center">
+            <Spinner size="lg" color="primary" />
+          </div>
+        ) : (
+          <DataTable data={data as any} />
+        )}
       </div>
     </main>
   )
